83b6e018905bd8d492de3118d4342ea5e509ea41
michealeeee <mke00007@gmail.com>
2026-01-13 05:26:29 +0000

    addedcountrycode

--- form.js
+++ form.js
@@ -1,12 +1,24 @@
 function submitForm(){
     const fullnameError = document.getElementById("fullnameError");
     const name = document.getElementById("name");
+    const namepattern = /^[A-Za-z\s]+$/;
     fullnameError.innerHTML="";
-    if (name.value==="" || name.value.length<2){
+    if (namepattern.test(name.value) === false){
+       fullnameError.innerHTML="Please fullname can only be alphabet";
+        name.focus();
+        return;  
+    }
+    if (name.value===""){
         fullnameError.innerHTML="Please enter your full name";
         name.focus();
         return;
+     }
+    if (name.value.length<2){
+        fullnameError.innerHTML="Full name should be more than two characters";
+        name.focus();
+        return;
     }
+
     const Gendererror = document.getElementById("Gendererror");
     const gender = document.getElementById("gender");
     Gendererror.innerHTML="";
@@ -17,11 +29,54 @@ function submitForm(){
     }
     const emailerror = document.getElementById("emailerror");
     const email = document.getElementById("email");
+    const emailpattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    // alert(emailpattern.test(email.value));
     emailerror.innerHTML="";
     if(email.value===""){
     emailerror.innerHTML="Please enter your email address";
     email.focus();
     return;
      }
+     if(emailpattern.test(email.value)===false){
+     emailerror.innerHTML="Invalid email format";
+     email.focus();
+    return;
+     }
+     const NumberError = document.getElementById("NumberError");
+     const phone = document.getElementById("phone");
+
+     const password = document.getElementById("password");
+     const passworderror = document.getElementById("PasswordError")
+    const passwordpattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/;
+    passworderror.innerHTML = "";
+    if(password.value===""){
+        passworderror.innerHTML = "Password cannot be empty, please set a new password";
+        password.focus();
+        return;
+    }
+    if( passwordpattern.test(password.value)===false){
+        passworderror.innerHTML = "Password must be at least one uppercase, one lowercase, one number, one special character, no spaces, and a minimum length (e.g., 8-16 chars)";
+        password.focus();
+        return;
+    }
+      const cpassword = document.getElementById("cpassword");
+     const cpassworderror = document.getElementById("cPasswordError")
+    const cpasswordpattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$/;
+    cpassworderror.innerHTML = "";
+    if(cpassword.value===""){
+        cpassworderror.innerHTML = "Password cannot be empty, please set a new password";
+        cpassword.focus();
+        return;
+    }
+    if( cpasswordpattern.test(cpassword.value)===false){
+        cpassworderror.innerHTML = "Password must be at least one uppercase, one lowercase, one number, one special character, no spaces, and a minimum length (e.g., 8-16 chars)";
+        cpassword.focus();
+        return;
+    }
+    if(cpassword.value !== password.value){
+    cpassworderror.innerHTML = "Both passwords are not the same, Please check and try again";
+        password.focus();
+        return;
+    }
 }
 
